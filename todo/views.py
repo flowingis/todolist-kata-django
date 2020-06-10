ac919e70c8d28618dd45c9a8510686091c7dc0b8
Massimo Biagioli <massimo.biagioli@ideato.info>
2020-06-10 14:16:14 +0200

    fix import

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import viewsets
 
 from todo.fake_permission import FakePermission
 from todo.models import Task
